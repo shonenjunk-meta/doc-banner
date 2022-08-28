2f53b3dc81ee5605c3ed3828c8b040b095a552a8
zombieboy98 <zeekillingmachine98@gmail.com>
2022-08-28 17:08:40 +0800

    update

--- web-app/components/Nav.tsx
+++ web-app/components/Nav.tsx
@@ -1,8 +1,6 @@
 import Link from 'next/link';
-import Image from 'next/image';
 import { useRouter } from 'next/router';
 import styles from './Nav.module.scss';
-import sjLogo from '../public/shonen-junk-logo.svg';
 
 export default function Nav() {
   const router = useRouter();
@@ -19,7 +17,7 @@ export default function Nav() {
       </Link>
       <Link href='/history'>
         <a className={currentRoute === '/history' ? styles.active : ''}>
-          HISTORY
+          History
         </a>
       </Link>
     </nav>
